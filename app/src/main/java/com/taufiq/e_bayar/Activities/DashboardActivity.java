71be95532e68fe753ac6c0caf91dcd76cc5f7f79
AhmadTaufiqR <e41211969@student.ac.id>
2023-04-10 08:58:10 +0700

    Guides and dashboard

--- app/src/main/java/com/taufiq/e_bayar/Activities/DashboardActivity.java
+++ app/src/main/java/com/taufiq/e_bayar/Activities/DashboardActivity.java
@@ -3,6 +3,7 @@ package com.taufiq.e_bayar.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.taufiq.e_bayar.R;
@@ -12,6 +13,7 @@ public class DashboardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_dashboard);
         btnNavView = findViewById(R.id.bottomNavigationView);
         btnNavView.setBackground(null);
--- app/src/main/java/com/taufiq/e_bayar/Activities/GuidesActivity.java
+++ app/src/main/java/com/taufiq/e_bayar/Activities/GuidesActivity.java
@@ -3,6 +3,7 @@ package com.taufiq.e_bayar.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.taufiq.e_bayar.R;
 
@@ -12,5 +13,6 @@ public class GuidesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guides);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
 }
