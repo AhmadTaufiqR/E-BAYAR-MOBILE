a16d6a217d81df9c1fdeb2f43d887c62fdea653b
developer <developer@polije.ac.id>
2023-05-22 09:01:56 +0700

    Membuat Tampilan Login

---
+++ app/src/main/java/com/taufiq/e_bayar/Activities/KodeVerifikasiActivity.java
@@ -0,0 +1,16 @@
+package com.taufiq.e_bayar.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.taufiq.e_bayar.R;
+
+public class KodeVerifikasiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kode_verifikasi);
+    }
+}
--- app/src/main/java/com/taufiq/e_bayar/Activities/ResetPasswordActivity.java
+++ app/src/main/java/com/taufiq/e_bayar/Activities/ResetPasswordActivity.java
@@ -1,14 +1,16 @@
-package com.taufiq.e_bayar;
+package com.taufiq.e_bayar.Activities;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.taufiq.e_bayar.R;
+
+public class ResetPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_reset_password);
     }
 }
